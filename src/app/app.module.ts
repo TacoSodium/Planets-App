e8440667b859b9b6a87a20aad06d7ab83527bfa4
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-09-03 13:49:18 +1000

    adding form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { PlanetsCreateComponent } from './planets-create/planets-create.componen
     PlanetsCreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/planet-detail/planet-detail.component.ts
+++ src/app/planet-detail/planet-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Planet } from "../models/planet";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-planet-detail',
@@ -14,13 +15,41 @@ export class PlanetDetailComponent implements OnInit {
   @Output()
   onDelete = new EventEmitter<Planet>();
 
+  toggleDefault: string = "Edit";
+  toggleText: string = this.toggleDefault;
+  editing: boolean = false;
+
+  planetForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.planetForm = new FormGroup({
+      name: new FormControl(this.planet.Name, Validators.required,),
+      moons: new FormControl(this.planet.Moons, [
+        Validators.required,
+        Validators.pattern('[0-9]*')
+      ]),
+      distance: new FormControl(this.planet.DistanceFromSun, [
+        Validators.required,
+        Validators.pattern('[0-9]*')
+      ])
+    });
   }
+
+  get name() { return this.planetForm.get('name'); }
 
   deletePlanet(remove: Planet) {
     this.onDelete.emit(remove);
   }
 
+  startEdit() {
+    this.editing = true;
+    this.toggleText = "Save";
+  }
+
+  stopEdit() {
+    this.editing = false;
+    this.toggleText = this.toggleDefault;
+  }
 }
